535fd3f294ea6581612074acf4ee4816b123c456
Jendrik Johannes <jendrik@gradle.com>
2016-12-05 13:14:49 +0100

    Remove unused imports

--- subprojects/dependency-management/src/main/java/org/gradle/api/internal/artifacts/transform/ArtifactAttributeMatcher.java
+++ subprojects/dependency-management/src/main/java/org/gradle/api/internal/artifacts/transform/ArtifactAttributeMatcher.java
@@ -16,14 +16,12 @@
 
 package org.gradle.api.internal.artifacts.transform;
 
-import org.gradle.api.attributes.Attribute;
 import org.gradle.api.attributes.AttributeContainer;
 import org.gradle.api.attributes.AttributesSchema;
 import org.gradle.api.attributes.HasAttributes;
 import org.gradle.internal.component.model.ComponentAttributeMatcher;
 
 import java.util.Collections;
-import java.util.Set;
 
 class ArtifactAttributeMatcher {
 
